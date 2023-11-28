e71bcad4e4a7f7c03ac6d5e62a0f682b100dac66
Shenay88 <sidirizova@gmail.com>
2023-11-28 10:25:51 +0000

    Create POST

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -1,17 +1,43 @@
+import { createBook } from '../../services/bookService';
+import {useNavigate} from 'react-router-dom';
 import './create.css';
 
 export default function Create() {
+
+    const navigate = useNavigate()
+
+    // Uncontrolled form
+    const createBookSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        // We get an object with all input values.
+        const bookData = Object.fromEntries(new FormData(e.currentTarget));
+
+        try {
+
+            // We get an object with all the data from the server, _id included
+            const newBook = await createBook(bookData);
+            navigate('/');
+
+        } catch (error) {
+
+            console.log(error)
+
+        }
+
+    }
+
     return (
-        <form className='createForm' action="#" method="post">
+        <form className='createForm' onSubmit={createBookSubmitHandler}>
             <h1>Add Book</h1>
             <div className="container">
 
-                <input type="text" name="name" className="title"  placeholder="Title"/>
-                <input type="text" name="image" className="image" placeholder="Image URL"/>
+                <input type="text" name="title" className="title" placeholder="Title" />
+                <input type="text" name="img" className="img" placeholder="Image URL" />
 
                 <div className="row">
                     <div className="half">
-                        <input type="number" name="year" className="year" placeholder='Year'/>
+                        <input type="number" name="year" className="year" placeholder='Year' />
                     </div>
 
                     <div className="half">
@@ -23,7 +49,7 @@ export default function Create() {
                     </div>
                 </div>
 
-                <input name="text" className="description" placeholder='Description'></input>
+                <textarea name="text" className="description" placeholder='Description'></textarea>
 
                 <input type="submit" value="Add Book" />
 
